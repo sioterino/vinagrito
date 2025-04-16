617965c4b8e2cee0347d795a46afcbf64dcf3b42
sofia <sofialvestoreti@gmail.com>
2025-04-16 13:28:23 -0300

    ajustes do css inicial

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// AQUI FICA A IMPLEMENTAÇÃO DAS CLASSES EU ACHO KKKKK
+// objetivo aqui é fazer só a importação desse arquivo para dentro do html
+
+document.querySelector("body").addEventListener("click", (e) => {
+    console.log(e.target)
+});
